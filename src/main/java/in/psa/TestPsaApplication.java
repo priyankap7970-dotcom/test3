c8e05ef45dfd48af486942a21467a93de09a07e9
priyanka <priyankap7970@gmail.com>
2026-01-09 21:11:24 +0530

    FIRST COMMIT

---
+++ src/main/java/in/psa/TestPsaApplication.java
@@ -0,0 +1,14 @@
+package in.psa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPsaApplication {
+
+	public static void main(String[] args) {
+        int x=100;
+        SpringApplication.run(TestPsaApplication.class, args);
+	}
+
+}
